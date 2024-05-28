685630842b1c9740a651ae543554eaf4eab7327e
rameen-danish <rameenrashid15@gmail.com>
2024-05-28 13:10:04 +0500

    first commit

---
+++ main.ts
@@ -0,0 +1,132 @@
+#! /usr/bin/env node
+//                                 ***Object Assignment***
+console.log("\n \t \t \t ***OBJECT ASSIGNMENT*** ");
+
+/*Part 1: Employee Data
+Challenge:
+1. Design a type alias named Employee to represent an employee object with properties like 
+name (string), department (string), and role (string).
+2. Include an optional nested object named contact to hold phone and email information (if 
+provided).
+3. Employ a union type for the role property to restrict it to "Manager", "Engineer", or 
+"Intern".
+4. Make the skills property an optional array of strings for listing an employee's skills (if 
+any).*/
+console.log("\n \t \t///***PART1 : Employee Data***///");
+
+type Employee = {                                                        //created a type Employee with its properties
+    name  : string ,
+    department : string ,
+    role : Role,
+    contact? : {                                                        //optional nested object with its properties
+        phone : number,
+        email : string,
+    }
+    skills? : string[],
+};
+
+type Role = "Manager"|"Engineer"|"Intern"|"Student";                       //created a union type Role with restrict properties
+
+let employee : Employee = {                                              //an emplpoyee object with its data
+    name: "Rameen",
+    department: "GIAIC department",
+    role: "Student",
+    contact : {
+        phone : 123456789,
+        email : "rameenrashid15@gmail.com"
+    },
+    skills : ["JavaScript","HTML","CSS"]
+};
+
+console.log(employee);                                               //print the employee object
+
+/*Part 2: Car Details
+Challenge:
+1. Design a type alias named Car to represent a car object.
+2. Include a nested object named engine within Car, containing a property named 
+horsepower (number).
+3. Define a function named getHorsepower directly within the Car type alias to retrieve the 
+engine's horsepower.*/
+console.log("\n \t \t///***PART2 : Car Details***///");
+
+type Car = {                                                    //creating a car type which represent car object
+    name : string ,
+    model : string ,
+    year : number ,
+    color : string ,
+    engine : {                                                     // engine is nested car object
+        horsePower : number ,
+    }
+    getHorsePower : () => any ;                                    // a function in  car object
+}
+ 
+let myCar : Car = {                                            //putting values in  car object
+    name: "Honda Civic",
+    model : "Standard",
+    year : 2024 ,
+    color : "Black",
+    engine : {
+        horsePower : 176,
+    },
+    getHorsePower : () => {
+        console.log(`${myCar.name} car speed is ${myCar.engine.horsePower} hp`);
+    }
+};
+myCar.getHorsePower();                                        //calling car object function
+console.log(myCar);
+
+/*Part 3: Colorful T-Shirts
+Challenge:
+1. Design a type alias named Product to represent a T-shirt object with properties like name
+(string), price (number), and color (string).
+2. Include a nested object named inventory within Product. This inventory object 
+should have two properties: 
+o stock (number): Represents the number of T-shirts available.
+o colorOptions (optional array of strings): Lists available colors (if any).
+3. Inside the inventory object, define a function named changeColor. This function 
+accepts a new color string as an argument. When called, it should: 
+o Update the color property of the Product object.
+o Adjust the price based on the new color (implement your own logic, e.g., 
+increase by 10% for red, decrease by 5% for blue)*/
+console.log("\n \t \t///***PART3 : Colourful T-Shirts***///");
+
+type Product = {                                                      //creating a type Product which represent T-shirt Object
+    name : string,
+    price : number,
+    color : string
+    inventory : {                                                   //nested object inventory
+        stock : number,                                             //Represents the number of T-shirts available.
+        colorOptions? : string[],                                   //optional array of strings for colorOptions
+        changeColor : (newColor:string) => void                     //change color function declaration
+    };
+}
+
+let shirt : Product = {                                              //values inserting in t-shirt object 
+    name : "POLO T-Shirt",
+    price : 2000,
+    color :"black",
+    inventory : {
+        stock : 100,
+        colorOptions : ["red","blue","white"],
+        changeColor : (newColor:string) => {                            //function implementation
+            shirt.color = newColor; 
+            shirt.price = 2000;                        
+            switch(newColor){
+                case "red":
+                    shirt.price *=1.1;                                   //Increase 10% for red colour
+                    break;
+                case "blue":
+                    shirt.price *= 0.95;                                   //Decrease 5% for blue colour
+                    break;
+                default:
+                    break;    
+            };
+        }
+    }
+}
+console.log(shirt);
+shirt.inventory.changeColor("red");                                                //function calling for red color
+console.log("\n \t For Red color T-shirt price increase by 10% \n",shirt);
+shirt.inventory.changeColor("blue");                                               //function calling for blue color
+console.log("\n \t For Blue color T-shirt price decrease by 5%",shirt);
+
